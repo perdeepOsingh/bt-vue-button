3bc80b3459cfebe772203f6091c8269536de81cf
Perdeep Singh <Perdeep.Singh@microba.com>
2019-07-24 14:18:02 +1000

    - Added eslint - Updated README.md - Added property actionInProgress - Trigger event when action in progress is updated.

--- bt-vue-button.vue
+++ bt-vue-button.vue
@@ -1,5 +1,4 @@
 <script>
-import 'bootstrap/dist/css/bootstrap.css'
 export default {
     name: 'bt-vue-button',
     props: {
@@ -30,11 +29,10 @@ export default {
                 // The value must match one of these strings
                 return ['success', 'failure', ''].indexOf(value) !== -1
             }
-        }
-    },
-    data() {
-        return {
-            actionInProgress: false
+        },
+        actionInProgress: {
+            type: Boolean,
+            default: false
         }
     },
     watch: {
@@ -43,7 +41,7 @@ export default {
             if(finalStateClass){
                 this.$el.querySelector('button').classList.add(finalStateClass)
                 setTimeout(() => {
-                    this.actionInProgress = false
+                    this.updateActionInProgress(false)
                     this.$el.querySelector('button').classList.remove(finalStateClass)
                 }, 2000);
             }
@@ -51,17 +49,17 @@ export default {
     },
     methods: {
         clickAction() {
-            this.actionInProgress = true
+            this.updateActionInProgress(true)
             this.$emit('clicked')
-            setTimeout(() => {
-                    this.actionInProgress = false
-                }, 2000);
+        },
+        updateActionInProgress (status) {
+            this.actionInProgress = status
+            this.$emit('update:actionInProgress', this.actionInProgress)
         }
     },
 }
 </script>
 <template>
-    <div>
         <button
         :name="btnName" 
         :id="btnId" 
@@ -72,5 +70,4 @@ export default {
             <span v-if="actionInProgress" class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
             {{btnValue}}
         </button>
-    </div>
 </template>
