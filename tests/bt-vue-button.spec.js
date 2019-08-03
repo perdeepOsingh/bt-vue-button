c61df47dc714dc26aaa0322fdb810e15ce717ed9
Perdeep Singh <Perdeep.Singh@microba.com>
2019-08-03 16:10:19 +1000

    - Added test configurations.

---
+++ tests/bt-vue-button.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from '@vue/test-utils'
+import btVueButton from '../bt-vue-button.vue'
+
+describe('Component bt-vue-button default', () => {
+    it('All data attributes are defined', () => {
+        const wrapper = shallowMount(btVueButton)
+        // console.log(wrapper)
+    })
+})
