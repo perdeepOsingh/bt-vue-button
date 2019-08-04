d9d8ab290fce320a061fc016bb04bd5bb95d6d00
Perdeep Singh <8705702+perdeepOsingh@users.noreply.github.com>
2019-08-04 10:07:56 +1000

    - Added tests.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
-};
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    describe: true,
+    beforeEach: true,
+    it: true,
+    expect: true,
+    jest: true
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
--- bt-vue-button.vue
+++ bt-vue-button.vue
@@ -1,90 +1,96 @@
 <script>
 export default {
-    name: 'bt-vue-button',
-    props: {
-        btnName: {
-            type: String,
-            default: 'btnVueButton-name'
-        },
-        btnId: {
-            type: String,
-            default: 'btnVueButton-Id'
-        },
-        btnClass: {
-            type: String,
-            default: null
-        },
-        btnValue: {
-            type: String,
-            default: 'Submit'
-        },
-        btnDisabled: {
-            type: Boolean,
-            default: false
-        },
-        btnFinalState: {
-            type: String,
-            default: null,
-            validator: function (value) {
-                // The value must match one of these strings
-                return ['success', 'failure', ''].indexOf(value) !== -1
-            }
-        },
-        actionInProgress: {
-            type: Boolean,
-            default: false
-        }
+  name: 'bt-vue-button',
+  props: {
+    btnName: {
+      type: String,
+      default: 'btnVueButton-name'
     },
-    data() {
-        return {
-            name: this.btnName,
-            id: this.btnId,
-            classes: this.btnClass,
-            value: this.btnValue,
-            disabled: this.btnDisabled,
-            finalState: this.btnFinalState,
-            inProgress: this.actionInProgress
-        }
+    btnId: {
+      type: String,
+      default: 'btnVueButton-Id'
     },
-    watch: {
-        btnFinalState(newValue) {
-            const finalStateClass = newValue && newValue.toLowerCase() === 'success' ? 'btn-success' : newValue && newValue.toLowerCase() === 'failure' ? 'btn-warning' : null
-            if(finalStateClass){
-                this.$refs.btVueButton.classList.add(finalStateClass)
-                this.updateActionInProgress(false)
-                setTimeout(() => {
-                    this.$refs.btVueButton.classList.remove(finalStateClass)
-                }, 2000);
-            }
-        },
-        actionInProgress() {
-            this.inProgress = this.actionInProgress
-        }
+    btnClass: {
+      type: String,
+      default: null
     },
-    methods: {
-        clickAction() {
-            this.updateActionInProgress(true)
-            this.$emit('clicked')
-        },
-        updateActionInProgress (status) {
-            this.inProgress = status
-            this.$emit('update:actionInProgress', this.inProgress)
-        }
+    btnValue: {
+      type: String,
+      default: 'Submit'
     },
+    btnDisabled: {
+      type: Boolean,
+      default: false
+    },
+    btnFinalState: {
+      type: String,
+      default: null,
+      validator: function (value) {
+        // The value must match one of these strings
+        return ['success', 'failure', ''].indexOf(value) !== -1
+      }
+    },
+    actionInProgress: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      name: this.btnName,
+      id: this.btnId,
+      classes: this.btnClass,
+      value: this.btnValue,
+      disabled: this.btnDisabled,
+      finalState: this.btnFinalState,
+      inProgress: this.actionInProgress
+    }
+  },
+  watch: {
+    btnFinalState (newValue) {
+      const finalStateClass =
+        newValue && newValue.toLowerCase() === 'success'
+          ? 'btn-success'
+          : newValue && newValue.toLowerCase() === 'failure'
+            ? 'btn-warning'
+            : null
+      if (finalStateClass) {
+        this.$refs.btVueButton.classList.add(finalStateClass)
+        this.updateActionInProgress(false)
+        setTimeout(() => {
+          this.$refs.btVueButton.classList.remove(finalStateClass)
+        }, 2000)
+      }
+    },
+    actionInProgress () {
+      this.inProgress = this.actionInProgress
+    }
+  },
+  methods: {
+    clickAction () {
+      this.updateActionInProgress(true)
+      this.$emit('clicked')
+    },
+    updateActionInProgress (status) {
+      this.inProgress = status
+      this.$emit('update:actionInProgress', this.inProgress)
+    }
+  }
 }
 </script>
 <template>
-        <button
-        ref="btVueButton"
-        :name="name" 
-        :id="id" 
-        :class="classes" 
-        :value="value"
-        :disabled="disabled || inProgress" 
-        @click="clickAction">
-            <slot name='inProgress' v-if="inProgress">
-                <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
-            </slot>
-            {{value}}
-        </button>
+  <button
+    ref="btVueButton"
+    :name="name"
+    :id="id"
+    :class="classes"
+    :value="value"
+    :disabled="disabled || inProgress"
+    @click="clickAction"
+  >
+    <slot name="inProgress" v-if="inProgress">
+      <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+    </slot>
+    {{value}}
+  </button>
 </template>
